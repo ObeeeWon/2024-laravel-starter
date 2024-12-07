ae65ec1ef2273fa33b1c4ebc7e65103e9e413d04
ObeeeWon <142458345+ObeeeWon@users.noreply.github.com>
2024-12-07 18:10:23 -0400

    Add the rest of attributes for items.

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -8,10 +8,30 @@
                     <div class="card-header">Create a item</div>
                     <div class="card-body">
 
-                        <form method="POST" action="/items">
+                        <form method="POST" action="/items">{{-- form-control gives round corners --}}
+
                             @csrf {{-- generate a token --}}
-                            <label for="item">item</label>{{-- form-control gives round corners --}}
-                            <input type="text" class="form-control" name="item" title="item" />
+                            <label for="category_id">category_id</label>
+                            <input type="text" class="form-control" name="category_id" title="category_id" />
+
+                            <label for="title">title</label>
+                            <input type="text" class="form-control" name="title" title="title" />
+
+                            <label for="description">description</label>
+                            <input type="text" class="form-control" name="description" title="description" />
+
+                            <label for="price">price</label>
+                            <input type="text" class="form-control" name="price" title="price" />
+
+                            <label for="quantity">quantity</label>
+                            <input type="text" class="form-control" name="quantity" title="quantity" />
+
+                            <label for="sku">sku</label>
+                            <input type="text" class="form-control" name="sku" title="sku" />
+
+                            <label for="picture">picture</label>
+                            <input type="text" class="form-control" name="picture" title="picture" />
+
                             <input type="submit" value="Add item"
                                    class="btn btn-primary btn-lg btn-block" style="margin-top:20px" />
                         </form>
